ba548c011c266247321871b22acf353594a21384
vishalchichani <vishalchichani@gmail.com>
2023-03-31 23:10:28 +0530

    authToken generated

--- backend/db.js
+++ backend/db.js
@@ -4,6 +4,7 @@ const mongoURI = "mongodb://localhost:27017/inotebook"
 
 const connectToMongo = () => {
     mongoose.connect( mongoURI)
+    .then(()=> console.log("Database connected."))
 }
 
 module.exports = connectToMongo;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken'); 
+const JWT_SECRET = "Yeahthatsit"; 
 
- 
 router.post('/createUser',[
     body('name','Length should atleast be 3 letters').isLength({min: 3}),
     body('email','Enter a valid email').isEmail(),
@@ -16,15 +18,30 @@ router.post('/createUser',[
     }
     try{
     let user = await User.findOne({email: req.body.email});
+
+    //Checking if user already exists
     if(user){
       return res.status(400).json({error: "Sorry! This email is already registered." });
     }
+
+    //Hashing the password
+    const salt = await bcrypt.genSalt(10);
+    const secpassword= await bcrypt.hash(req.body.password,salt);
+
     user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secpassword
       })
-      res.json("Done!"); 
+    const data = {
+      user:{
+        id: user.id
+      }
+    }
+
+    const jwtData = jwt.sign(data, JWT_SECRET);   
+    res.json(jwtData); 
+    
     } catch(error) {
       console.error(error.message);
       res.status(501).send("Unknown error occured.");
