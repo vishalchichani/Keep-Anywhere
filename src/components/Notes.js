39b8e619227a20193c4fc8a6bbcf6369353c0393
vishalchichani <vishalchichani@gmail.com>
2023-04-11 23:18:43 +0530

    noteitem component added

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,17 +1,17 @@
 
 import React, {useContext} from 'react'
 import noteContext from '../context/notes/noteContext'
-
+import NoteItem from './NoteItem';
 const Notes = () => {
     const context = useContext(noteContext);
     const {notes, setNotes} = context;
 
     return (
 
-        <div className='container my-4'>
+        <div className='row my-4'>
             <h2>Your Notes</h2>
             {notes && notes.map((note) => {
-                return note.title
+                return <NoteItem note= {note} />
             })}
 
         </div>
