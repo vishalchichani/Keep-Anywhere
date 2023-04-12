54d0ea8a56b4ceab2f3fcb9d8af260603dd43562
vishalchichani <vishalchichani@gmail.com>
2023-04-13 01:50:16 +0530

    about page added

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,50 @@ import React from 'react'
 
 function About() {
   return (
-    <div>This is About</div>
+    <div className='container my-4 mx-3'>
+      <h1>iNoteBook</h1>
+
+<p>It is a note-keeper app that will help you store your tasks online so you can access it from anywhere. 
+
+<br />Your Notes are secured and can only be accessed by authorized user.</p>
+<hr />
+<br />
+<h2> Tech-Stacks Used </h2>
+
+<p>The following Tech-Stacks and Libraries have been used in this project.<br />
+ <ul>
+  <li>React</li>
+ <li>BootStrap</li>
+ <li>Nodejs</li>
+ <li>Express</li>
+ <li>MongoDB</li>
+ <li>Mongoose</li>
+ </ul>
+ </p>
+ 
+ <hr />
+<br />
+<h3> More About App </h3>
+<p>You can get started in 3 easy steps:<br />
+<ol>
+  <li> Signup with your credentials.</li>
+<li> Login with valid credentials.</li>
+<li> Start creating the Notes. </li>
+</ol>
+
+You can add new notes, edit old notes, and delete existing notes.</p>
+<hr />
+<br />
+<h4> Further Ideas </h4>
+
+<p>
+<ul>
+  <li> We can add the details of user on a sideboard.</li>
+<li> Proper designing of the app can be done.</li>
+<li> Strikethrough or checking out functionality of redundant notes can be added.</li>
+</ul>
+</p>
+    </div>
   )
 }
 
